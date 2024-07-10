93ef30ecbe1d994ebe592af994807f199bb49ec6
seanmabli <seanmabli4@gmail.com>
2024-07-10 18:46:07 +0000

    caching

--- app/[blogId]/page.tsx
+++ app/[blogId]/page.tsx
@@ -1,58 +1,24 @@
-'use client';
-
-
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
 import { parse } from 'node-html-parser';
-import { Skeleton } from "@/components/ui/skeleton"
 
-import './page.css';
-
-const apiUrl = 'https://api.synodic.ai';
-
-export default function BlogPost({ params }: { params: { blogId: string } }) {
-  const [htmlContent, setHtmlContent] = useState<string | null>(null);
-
-  const cleanHtmlContent = (htmlContent: string | null): string | null => {
-    if (!htmlContent) {
-      return null;
+async function getData(blogId: string) {
+  const response = await fetch(`https://api.synodic.ai/campaigns/${blogId}`, { cache: 'force-cache' });
+  const data = await response.json();
+  const root = parse(data.html);
+  root.querySelectorAll('td.mceLayoutContainer').forEach((tag: any) => {
+    if (
+      tag.innerText.includes('View email in browser') ||
+      tag.innerText.includes('update your preferences') ||
+      tag.innerText.includes('unsubscribe')
+    ) {
+      tag.remove();
     }
+  });
+  return root.toString();
+}
 
-    const root = parse(htmlContent);
+export default async function BlogPost({ params }: { params: { blogId: string } }) {
+  const htmlContent = await getData(params.blogId);
 
-    root.querySelectorAll('td.mceLayoutContainer').forEach((tag) => {
-      if (
-        tag.innerText.includes('View email in browser') ||
-        tag.innerText.includes('update your preferences') ||
-        tag.innerText.includes('unsubscribe')
-      ) {
-        tag.remove();
-      }
-    });
-
-    return root.toString();
-  };
-
-  useEffect(() => {
-    const fetchCampaignHtml = async (campaignId: string) => {
-      const url = `${apiUrl}/campaigns/${campaignId}`;
-      const response = await axios.get(url);
-      
-      if (response.status === 200) {
-        const cleanedHtmlContent = cleanHtmlContent(response.data.html);
-        setHtmlContent(cleanedHtmlContent || '');
-      } else {
-        console.error(`Failed to retrieve content for campaign ${campaignId}: ${response.status}, ${response.statusText}`);
-        setHtmlContent(null);
-      }
-    };
-
-    fetchCampaignHtml(params.blogId);
-  }, [params.blogId]);
-
-  if (!htmlContent) {
-    return <div className="flex flex-col items-center"><Skeleton className="mb-5 h-[100vh] w-[660px]" /></div>;
-  }
   return (
       <div dangerouslySetInnerHTML={{ __html: htmlContent }} />
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,86 @@
-'use client';
+// "use client"
+// import { useState } from 'react';
+import Link from 'next/link';
 
-import React from 'react';
-import Campaigns from './mailchimp';
-
+import { Skeleton } from "@/components/ui/skeleton";
 import {
-  Pagination,
-  PaginationContent,
-  PaginationEllipsis,
-  PaginationItem,
-  PaginationLink,
-  PaginationNext,
-  PaginationPrevious,
-} from "@/components/ui/pagination"
+    Card,
+    CardContent,
+    CardDescription,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card";
+// import { CustomPagination } from '@/components/pagination';
 
-const Page: React.FC = () => {
-  return (
-    <div>
-      <Campaigns />
-    </div>
-  );
+async function getData() {
+    const response = await fetch("https://api.synodic.ai/campaigns", { cache: 'force-cache' });
+    const data = await response.json();
+    const campaigns = data.campaigns
+        .filter((campaign: any) => campaign.settings?.preview_text)
+        .sort((a: any, b: any) => new Date(b.send_time).getTime() - new Date(a.send_time).getTime());
+    return campaigns;
+}
+
+export default async function Page() {
+    const campaigns = await getData();
+    
+    return (
+        <div className='min-h-screen'>
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-4 p-4 ">
+                {
+                    campaigns.length === 0 ? (
+                        <>
+                            {[...Array(6)].map((_, index) => (
+                                <Card key={index}>
+                                    <CardHeader>
+                                        <CardTitle><Skeleton className="h-[24px] w-[250px]" /></CardTitle>
+                                        <CardDescription><Skeleton className="h-[20px] w-[350px]" /></CardDescription>
+                                    </CardHeader>
+                                    <CardContent>
+                                        <Skeleton className="h-[24px]" />
+                                    </CardContent>
+                                </Card>
+                            ))}
+                        </>
+                    ) : (
+                        <>
+                            {campaigns.length > 0 && campaigns.map((campaign: any, index: number) => (
+                                <div className="cursor-pointer" key={campaign.id}>
+                                    <Link href={`/${campaign.id}`}>
+                                        <Card>
+                                            <CardHeader>
+                                                <CardTitle>{campaign.settings?.subject_line || 'Untitled Campaign'}</CardTitle>
+                                                <CardDescription>{campaign.settings?.preview_text || 'Untitled Campaign'}</CardDescription>
+                                            </CardHeader>
+                                        </Card>
+                                    </Link>
+                                </div>
+                            ))}
+                        </>
+                    )
+                }
+            </div>
+        </div>
+    );
+}
+
+
+
+// const [currentPage, setCurrentPage] = useState(0);
+
+/*
+const formatDate = (dateString: string | undefined): string => {
+    if (!dateString) return '';
+    const date = new Date(dateString);
+    return date.toISOString().split('T')[0];
 };
+*/
 
-export default Page;
+
+/*
+                                ?.slice(currentPage * 6, (currentPage + 1) * 6)
+                                    <CardContent>
+                                        <p>Posted {formatDate(campaign.send_time)}</p>
+                                    </CardContent>
+<CustomPagination pages={Math.ceil((campaigns?.length || 0) / 6)} setCurrentPage={setCurrentPage} />
+*/
